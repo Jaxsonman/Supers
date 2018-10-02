96de53dcb00c9d3549a3e8e13dc8c66ee54cb9db
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-02 10:00:14 -0600

    made better

--- src/components/Supers/supers_list.js
+++ src/components/Supers/supers_list.js
@@ -1,24 +1,11 @@
 import React, {Component} from 'react';
 
-const rawSupersData = [
-    {title: 'Post One', content: 'Post content', status: 'draft'},
-    {title: 'Post Two', content: 'Post content', status: 'published'},
-    {title: 'Post Three', content: 'Post content', status: 'published'},
-    {title: 'Post Four', content: 'Post content', status: 'published'},
-
-]
 
 export default class SupersList extends Component {
     constructor(props) {
         super();
-
-        this.state = {
-            journalData: rawSupersData,
-            greeting: 'Hi there',
-            isOpen: true
-        };
     }
     render() {
         return <h2>{this.props.heading}</h2>;
     }
-}
+} 
