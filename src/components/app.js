21f53a54cc161313f817b942b91d4e38ca52b7b8
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-01 22:03:31 -0600

    First Commit

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import SupersList from './Supers/supers_list';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Supers List</h1>
+        <h2>Top Three Favorite Supers.</h2>
+        <SupersList heading ="SpiderMan"/>
+        <SupersList heading ="Venom"/>
+        <SupersList heading ="Thor"/>
+      </div>
+    );
+  }
+}
